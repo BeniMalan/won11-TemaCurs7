81446ec7f51ba23ef6b4c44647f2120bd23d49b8
BeniMalan <malanbeniaminflorin@gmail.com>
2023-03-19 22:25:21 +0200

    Tema curs 7

--- src/Student/Main.java
+++ src/Student/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main (String[] args){
         student st1=new student("Ion",9);
         student st2=new student("Vlad",10);
-        System.out.println("Numele "+st1.getName()+"Nota "+st1.getGrade());
-        System.out.println("Numele "+st2.getName()+"Nota "+st2.getGrade());
+        System.out.println("Numele :"+st1.getName()+"; Nota "+st1.getGrade());
+        System.out.println("Numele :"+st2.getName()+"; Nota "+st2.getGrade());
 
         double averageGrade = student.getAverageStudentGrade(student.sumOfGrades, student.numberOfStudents);
-        System.out.println("Media: " + averageGrade);
+        System.out.println("Media notelor studentilor: " + averageGrade);
     }
 }
